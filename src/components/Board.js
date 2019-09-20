170d77b276f11f16038678119618dbd83e4e1517
Francisco Costa <francisco@fcosta.pt>
2019-09-20 22:43:54 +0100

    Created Navbar, Board and NewsCard components. Added material design.

---
+++ src/components/Board.js
@@ -0,0 +1,30 @@
+import React from 'react'
+import Grid from '@material-ui/core/Grid';
+import { makeStyles } from '@material-ui/core/styles';
+
+import NewsCard from "./NewsCard"
+
+const useStyles = makeStyles(theme => ({
+    root: {
+      flexGrow: 1,
+    },
+    paper: {
+      padding: theme.spacing(2),
+      textAlign: 'center',
+      color: theme.palette.text.secondary,
+    },
+  }));
+
+const Board = () => {
+    const classes = useStyles();
+
+  return (
+    <div className={classes.root}>
+      <Grid container spacing={3}>
+        <NewsCard />
+      </Grid>
+    </div>
+  );
+}
+
+export default Board
